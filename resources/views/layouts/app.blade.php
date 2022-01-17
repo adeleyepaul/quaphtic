e336d0dd5fbd5239ccf95690feb68d6ee4af8f98
Paul Adeleye <adeleyepau2@gmail.com>
2022-01-17 22:44:44 +0100

    first commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,234 @@
+<!DOCTYPE html>
+<html lang="en">
+  
+<!-- Mirrored from themes.potenzaglobalsolutions.com/html/hi-soft/ by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 29 Dec 2021 11:53:34 GMT -->
+<head>
+    <meta charset="utf-8">
+    <meta name="keywords" content="HTML5 Template" />
+    <meta name="description" content="Hi-soft - IT Solutions and Services Company HTML5 Template" />
+    <meta name="author" content="potenzaglobalsolutions.com" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Quaphtic Services Limited</title>
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="images/favicon.ico" />
+
+    <!-- Google Font -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Archivo:400,400i,500,500i,600,600i,700,700i&amp;display=swap">
+
+    <!-- CSS Global Compulsory (Do not remove)-->
+    <link rel="stylesheet" href="css/font-awesome/all.min.css" />
+    <link rel="stylesheet" href="css/flaticon/flaticon.css" />
+    <link rel="stylesheet" href="css/bootstrap/bootstrap.min.css" />
+
+    <!-- Page CSS Implementing Plugins (Remove the plugin CSS here if site does not use that feature)-->
+    <link rel="stylesheet" href="css/owl-carousel/owl.carousel.min.css" />
+    <link rel="stylesheet" href="css/swiper/swiper.min.css" />
+    <link rel="stylesheet" href="css/animate/animate.min.css"/>
+    <link rel="stylesheet" href="css/magnific-popup/magnific-popup.css" />
+
+    <!-- Template Style -->
+    <link rel="stylesheet" href="css/style.css" />
+
+  </head>
+  <body>
+
+    <!--=================================
+    header -->
+    <header class="header default">
+      <div class="topbar">
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-12">
+              <div class="d-block d-md-flex align-items-center text-center">
+                <div class="me-4 d-inline-block py-1">
+                  <a href="mailto:info@quaphtic.com"><i class="far fa-envelope me-2 fa-flip-horizontal text-primary"></i>info@quaphtic.com</a>
+                </div>
+                <div class="me-auto d-inline-block py-1">
+                  <a href="#"><i class="fas fa-map-marker-alt text-primary me-2"></i>18, Medical Road, Off Obafemi Awolowo Road, Ikeja, Lagos State. Nigeria</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <nav class="navbar bg-white navbar-static-top navbar-expand-lg">
+        <div class="container-fluid">
+          <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target=".navbar-collapse"><i class="fas fa-align-left"></i></button>
+          <a class="navbar-brand" href="/">
+            <img class="img-fluid" src="images/quaphtic-logo.png" alt="logo">
+          </a>
+          <div class="navbar-collapse collapse">
+            <ul class="nav navbar-nav">
+              <li class="nav-item active">
+                <a class="nav-link" href="#" aria-haspopup="true" aria-expanded="false">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#" aria-haspopup="true" aria-expanded="fale">Services</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#" aria-haspopup="true" aria-expanded="fale">About Us</a>
+              </li>
+			  <li class="nav-item">
+                <a class="nav-link" href="#" aria-haspopup="true" aria-expanded="fale">Contact Us</a>
+              </li>
+            </ul>
+          </div>
+          <div class="d-none d-sm-flex ms-auto me-5 me-lg-0 pe-4 pe-lg-0">
+            <ul class="nav ms-1 ms-lg-2 align-self-center">
+              <li class="contact-number nav-item pe-4 ">
+                <a class="fw-bold" href="#"><i class="fab fa-whatsapp pe-2"></i>+234 802 7132 536</a>
+              </li>
+              <li class="header-search nav-item">
+                <div class="search">
+                  <a class="search-btn not_click" href="javascript:void(0);"></a>
+                  <div class="search-box not-click">
+                    <form action="https://themes.potenzaglobalsolutions.com/html/hi-soft/search.html" method="get">
+                      <input type="text" class="not-click form-control" name="search" placeholder="Search..">
+                      <button class="search-button" type="submit"> <i class="fa fa-search not-click"></i></button>
+                    </form>
+                  </div>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
+    <!--=================================
+    header -->
+
+    @yield('content')
+
+    
+    <!--=================================
+    footer-->
+    <footer class="footer">
+        <div class="footer-top">
+          <div class="container">
+            <div class="row align-items-center">
+              <div class="col-sm-3">
+                <a href="index-2.html"><img class="img-fluid" src="images/quaphtic-logo.png" alt="logo"></a>
+              </div>
+              <div class="col-sm-9 text-sm-end mt-4 mt-sm-0">
+                <ul class="list-unstyled mb-0 social-icon">
+                  <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                  <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                  <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
+                  <li><a href="#"><i class="fab fa-github"></i></a></li>
+                  <li><a href="#"><i class="fab fa-dribbble"></i></a></li>
+                  <li><a href="#"><i class="fab fa-behance"></i></a></li>
+                </ul>
+              </div>
+            </div>
+            <hr class="my-4 my-sm-5 pb-0">
+          </div>
+        </div>
+        <div class="container">
+          <div class="row">
+            <div class="col-sm-6 col-lg-4 mb-4 mb-lg-0">
+              <h5 class="text-primary mb-2 mb-sm-4">IT Services</h5>
+              <div class="footer-link">
+                <ul class="list-unstyled mb-0">
+                  <li><a href="#">Data Synchronization</a></li>
+                  <li><a href="#">Content Management</a></li>
+                  <li><a href="#">Content Delivery</a></li>
+                  <li><a href="#">Transaction Processing</a></li>
+                  <li><a href="#">Process Automation</a></li>
+                </ul>
+                <ul class="list-unstyled mb-0">
+                  <li><a href="#">Event Processing</a></li>
+                  <li><a href="#">Information Security</a></li>
+                  <li><a href="#">Mobile Platforms</a></li>
+                  <li><a href="#">Communications</a></li>
+                </ul>
+              </div>
+            </div>
+            <div class="col-sm-6 col-lg-2 mb-4 mb-lg-0">
+              <h5 class="text-primary mb-2 mb-sm-4">Company</h5>
+              <div class="footer-link">
+                <ul class="list-unstyled mb-0">
+                  <li><a href="#">About</a></li>
+                  <li><a href="#">Leadership Team</a></li>
+                  <li><a href="#">Case Studies</a></li>
+                  <li><a href="#">Locations</a></li>
+                  <li><a href="#">Careers <span class="badge bg-success ms-2">We're hiring</span></a></li>
+                </ul>
+              </div>
+            </div>
+            <div class="col-sm-6 col-lg-2 mb-4 mb-sm-0">
+              <h5 class="text-primary mb-2 mb-sm-4">Support</h5>
+              <div class="footer-link">
+                <ul class="list-unstyled mb-0">
+                  <li><a href="#">Forum Support</a></li>
+                  <li><a href="#">Help & FAQs</a></li>
+                  <li><a href="#">Contact Us</a></li>
+                  <li><a href="#">Pricing And Plans</a></li>
+                  <li><a href="#">Cookies Policy</a></li>
+                  <li><a href="#">Privacy Policy</a></li>
+                </ul>
+              </div>
+            </div>
+            <div class="col-sm-6 col-lg-4">
+              <div class="footer-contact-info">
+                <h5 class="text-primary mb-3">Contact Quaphtic</h5>
+                <div class="contact-address">
+                  <div class="contact-item">
+                    <label>Address:</label>
+                    <p>18, Medical Road, Off Obafemi Awolowo Road, Ikeja, Lagos State. Nigeria</p>
+                  </div>
+                  <div class="contact-item">
+                    <label>Phone:</label>
+                    <h5 class="mb-0 fw-bold"><a href="#">+234 807 132 536</a></h5>
+                  </div>
+                  <div class="contact-item">
+                    <label>Email:</label>
+                    <a class="text-dark" href="mailto:info@quaphtic.com">info@quaphtic.com</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="footer-bottom py-sm-5 py-4">
+          <div class="container">
+            <div class="row">
+              <div class="col-lg-12 text-center">
+                <p class="mb-0">©Copyright {{ date('Y') }} <a href="index-2.html">Quaphtic Services Limited</a> All Rights Reserved</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+      <!--=================================
+      footer-->
+  
+      <!--=================================
+      Back To Top-->
+      <div id="back-to-top" class="back-to-top">up</div>
+      <!--=================================
+      Back To Top-->
+  
+      <!--=================================
+      Javascript -->
+  
+      <!-- JS Global Compulsory (Do not remove)-->
+      <script src="js/jquery-3.6.0.min.js"></script>
+      <script src="js/popper/popper.min.js"></script>
+      <script src="js/bootstrap/bootstrap.min.js"></script>
+  
+      <!-- Page JS Implementing Plugins (Remove the plugin script here if site does not use that feature)-->
+      <script src="js/jquery.appear.js"></script>
+      <script src="js/swiper/swiper.min.js"></script>
+      <script src="js/swiperanimation/SwiperAnimation.min.js"></script>
+      <script src="js/counter/jquery.countTo.js"></script>
+      <script src="js/owl-carousel/owl.carousel.min.js"></script>
+      <script src="js/magnific-popup/jquery.magnific-popup.min.js"></script>
+  
+      <!-- Template Scripts (Do not remove)-->
+      <script src="js/custom.js"></script>
+  
+    </body>
+  
+  <!-- Mirrored from themes.potenzaglobalsolutions.com/html/hi-soft/ by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 29 Dec 2021 11:54:04 GMT -->
+  </html>
